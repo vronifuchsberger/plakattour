33b1ebd640c62c0911048425f3597a6658a199fd
vronifuchsberger <vronifuchsberger@googlemail.com>
2019-06-22 16:26:55 +0100

    Share

---
+++ components/Share.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { Text, View, TouchableHighlight, StyleSheet, Share } from "react-native";
+import { EvilIcons } from "@expo/vector-icons";
+
+export default class ListItem extends React.Component {
+
+  async onShare = () => {
+      try {
+        const result = await Share.share({
+          message:
+            'Someday this will be a url to the export file',
+        })
+
+        if (result.action === Share.sharedAction) {
+          if (result.activityType) {
+            // shared with activity type of result.activityType
+          } else {
+            // shared
+          }
+        } else if (result.action === Share.dismissedAction) {
+          // dismissed
+        }
+      } catch (error) {
+        alert(error.message);
+      }
+    };
+
+  render() {
+    return (
+      <View style={styles.wrapper}>
+        <TouchableHighlight style={styles.innerWrapper} onPress={this.onShare}>
+          <EvilIcons
+            name="share-apple"
+            size={26}
+            color="#C7C7CC"
+            style={styles.shareIcon}
+          />
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: "red"
+  },
+  shareIcon: {}
+});
